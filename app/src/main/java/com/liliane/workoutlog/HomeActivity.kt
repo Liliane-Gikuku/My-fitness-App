527d35b5f01f7632cd75d99e79e6d117c6b84b9e
Liliane-Gikuku <lilkundiro@gmail.com>
2022-06-16 19:36:18 +0300

    Connecting bottom nav

--- app/src/main/java/com/liliane/workoutlog/HomeActivity.kt
+++ app/src/main/java/com/liliane/workoutlog/HomeActivity.kt
@@ -2,10 +2,46 @@ package com.liliane.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bnvHome: BottomNavigationView
+    lateinit var fcvHome: FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+
+    }
+    fun castViews(){
+        bnvHome=findViewById(R.id.bottom_nav)
+        fcvHome=findViewById(R.id.fcvHome)
+    }
+    fun setupBottomNav(){
+        bnvHome.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.plan ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome,PlanFragment()).commit()
+                    true
+                }
+                R.id.track ->{
+                    var transaction=supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome,TrackFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.profile->{
+                    var transaction=supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome,ProfileFragment())
+                    transaction.commit()
+                    true
+
+                }
+                else->false
+            }
+        }
     }
 }
